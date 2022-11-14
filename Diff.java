116a135d267bb9236ece2a02328d629ef6510ef3
Akshay <Akshaysanap2123@gmail.com>
2022-11-14 16:45:43 +0530

    Adding diff

---
+++ Diff.java
@@ -0,0 +1,8 @@
+public class Diff
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Nothing to print");
+	}
+}
+
--- sum.java
+++ sum.java
@@ -1,11 +1,12 @@
-public class sum
+public class Sum
 {
 	public static void main(String[] args)
 	{
-		int a = 10;
-		int b = 15;
+		int a = 5;
+		int b = 10;
+		int c=a+b;
 
-		System.out.println("A + b : "+(a+b));
+		System.out.println("Sum of a+b: "+c);
 	}
 }
 
